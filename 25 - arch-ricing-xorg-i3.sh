5b25be7cc648c285287351d6597ed9c0c6408e60
Kevin Baisas <kevin.baisas@gmail.com>
2019-03-14 02:46:03 +0800

    changes on scripts

--- 25 - arch-ricing-xorg-i3.sh
+++ 25 - arch-ricing-xorg-i3.sh
@@ -1,6 +1,194 @@
 
 #!/bin/bash
 
+
+# install AUR helper: yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+yes | makepkg --syncdeps --install
+yes | yay -Syu
+cd ..
+rm -rf yay
+
+# Sound
+yes | sudo pacman -S alsa-utils
+
+amixer sset "Master" unmute
+amixer sset "Speaker" unmute
+amixer sset "Headphone" unmute
+
+amixer sset "Master" 100%
+amixer sset "Speaker" 100%
+amixer sset "Headphone" 100%
+amixer sset "Mic Boost" 100%
+
+# Gstreamer
+yes | sudo pacman -S gstreamer
+yes | sudo pacman -S clutter-gst
+yes | sudo pacman -S gst-libav
+yes | sudo pacman -S gst-plugins-bad
+yes | sudo pacman -S gst-plugins-base
+yes | sudo pacman -S gst-plugins-base-libs
+yes | sudo pacman -S gst-plugins-good
+yes | sudo pacman -S gst-plugins-ugly
+
+# Browser packages
+yes | sudo pacman -S jre-openjdk flashplugin pepper-flash
+yes | yay -S ttf-ms-fonts --noconfirm
+
+## Hardware acceleration drivers installation
+gpu=;
+while true; do
+  echo "Your GPU: ";
+  lspci -k | grep -A 2 -E "(VGA|3D)";
+  read -p "
+
+
+What GPU are you using? [i]ntel | [a]md | [n]vidia | [v]m | [e]xit   " gpui
+  case $gpui in
+    [Ii]* )
+      while true; do
+        read -p "
+
+Model?
+Check: https://en.wikipedia.org/wiki/Intel_Graphics_Technology
+[1] Broadwell and newer
+[2] GMA 4500 series and newer GPUs up to Coffee Lake
+[e]xit
+    " ihva
+        case $ihva in
+          [1] )
+            gpu=intel;
+            yes | sudo pacman -S intel-media-driver;
+            break 2;;
+          [2] )
+            gpu=intel;
+            yes | sudo pacman -S libva-intel-driver intel-media-driver;
+            break 2;;
+          [Ee]* ) break;;
+          * ) echo Invalid input;
+        esac
+      done;;
+    [Aa]* )
+      while true; do
+        read -p "
+
+Model?
+Check: https://en.wikipedia.org/wiki/Template:AMD_graphics_API_support
+[1] Radeon R300 and newer
+[2] Radeon HD 2000 and newer
+[e]xit
+    " ihva
+        case $ihva in
+          [1] )
+            gpu=amd;
+            yes | sudo pacman -S mesa-vdpau lib32-mesa-vdpau;
+            break 2;;
+          [2] )
+            gpu=amd;
+            yes | sudo pacman -S mesa-vdpau lib32-mesa-vdpau;
+            yes | sudo pacman -S libva-mesa-driver lib32-libva-mesa-driver;
+            break 2;;
+          [Ee]* ) break;;
+          * ) echo Invalid input;
+        esac
+      done;;
+    [Nn]* )
+      gpu=nvidia;
+      yes | sudo pacman -S nvidia-utils libva-mesa-driver mesa-vdpau;
+      yes | yay -S nouveau-fw;
+      break;;
+    [Vv]* )
+      gpu=vm;
+      break;;
+    [Ee]* ) break;;
+    * ) echo Invalid input
+  esac
+done
+
+## Fallback hardware video acceleration
+yes | sudo pacman -S libva-vdpau-driver libvdpau-va-gl;
+
+while true; do
+  lspci -nnk | grep 0280 -A3
+  read -p "
+
+
+Wireless drivers installation:
+If your driver is not listed, check:
+https://wiki.archlinux.org/index.php/Wireless_network_configuration
+
+[1] Show Network Controller
+[2] Broadcom
+[3] Realtek R8168
+[4] Realtek RTL8188EUS (RTL8188EUS, RTL8188ETV)
+[5] Realtek RTL8188CUS (8188C, 8192C) 
+[m] Modprobe a module
+[e] Exit
+  " wd
+  case $wd in
+    [Ee]* ) break;;
+    [Mm]* )
+      while true; do
+        read -p "Enter module:   " m
+        case $m in
+          * ) sudo modprobe $m; break;;
+        esac
+      done;;
+    [1] ) lspci | grep Network;;
+    [2] ) sudo pacman -S broadcom-wl-dkms; echo "
+Installation done...
+";;
+    [3] ) sudo pacman -S r8168; echo "
+Installation done...
+";;
+    [4] ) yay -S 8188eu-dkms; echo "
+Installation done...
+";;
+    [5] ) yay -S 8192cu-dkms; echo "
+Installation done...
+";;
+  esac
+done
+
+while true; do
+  echo "
+
+
+For details, read:
+https://wiki.archlinux.org/index.php/Advanced_Linux_Sound_Architecture#Correctly_detect_microphone_plugged_in_a_4-pin_3.5mm_(TRRS)_jack
+
+"
+  read -p "Detect microphone plugged in a 4-pin 3.5mm (TRRS) jack [Yn]?   " yn
+  case $yn in
+    [Nn]* ) break;;
+    * )
+      while true; do
+        echo "
+Devices:
+"
+        aplay --list-devices
+        read -p "
+
+
+Check your HD Audio model here:
+https://dri.freedesktop.org/docs/drm/sound/hd-audio/models.html or
+http://git.alsa-project.org/?p=alsa-kernel.git;a=blob;f=Documentation/sound/alsa/HD-Audio-Models.txt;hb=HEAD
+
+Enter HD Audio Model (e.g. mbp101, macbook-pro, laptop-dmic etc) or [e]xit:   " hdam
+        case $hdam in
+          [Ee] ) break 2;;
+          * )
+            sudo touch /etc/modprobe.d/alsa-base.conf;
+            echo "
+options snd_hda_intel index=0
+options snd_hda_intel model=$hdam" | sudo tee /etc/modprobe.d/alsa-base.conf
+            break;;
+        esac
+      done;;
+  esac
+done
+
 # Install display manager
 yes | sudo pacman -S lightdm
 yes | yay -S lightdm-settings
