0b1a088f7ab914f5e0d1251e0ad0991bb3bc64a2
Kevin Baisas <kevin.baisas@gmail.com>
2019-03-20 00:17:33 +0800

    update lightdm handling

--- 25 - debian-ricing-xorg-i3.sh
+++ 25 - debian-ricing-xorg-i3.sh
@@ -494,16 +494,21 @@ else
   fi
 fi
 
-if cat $lightdmUnit | grep -q 'Alias=display-manager.service'; then
-  echo 'Alias already exists'
-else
-  if cat $lightdmUnit | grep -q '\[Install\]'; then
-    echo 'Install already exists'
+# If lightdm unit doesnt exists it may be manage by other unit.
+# In ubuntu, ubuntu lets you pick your default display manager when lightdm is installed,
+# instead of settings a daemon
+if [ -f $lightdmUnit ]; then
+  if cat $lightdmUnit | grep -q 'Alias=display-manager.service'; then
+    echo 'Alias already exists'
   else
-    echo '[Install]' | sudo tee -a $lightdmUnit
-  fi
+    if cat $lightdmUnit | grep -q '\[Install\]'; then
+      echo 'Install already exists'
+    else
+      echo '[Install]' | sudo tee -a $lightdmUnit
+    fi
 
-  echo 'Alias=display-manager.service' | sudo tee -a $lightdmUnit
+    echo 'Alias=display-manager.service' | sudo tee -a $lightdmUnit
+  fi
 fi
 
 sudo systemctl enable lightdm
