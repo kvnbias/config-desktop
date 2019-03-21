5e9c13cc430017e2ba8b9683aeb33965d45b14c7
Kevin Baisas <kevin.baisas@gmail.com>
2019-03-21 21:25:55 +0800

    add polkit location for debian systems

--- scripts/polkit-launch.sh
+++ scripts/polkit-launch.sh
@@ -5,6 +5,8 @@ if [ -f /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 ]; then
   /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 elif [ -f /usr/libexec/polkit-gnome-authentication-agent-1 ]; then
   /usr/libexec/polkit-gnome-authentication-agent-1 &
+elif [ -f /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 ]; then
+  /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 else
   echo "No polkit initialized."
 fi
