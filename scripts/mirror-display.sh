c1ecaa5181664620c971a2ea1505b8af081723e1
Kevin Baisas <kevin.baisas@gmail.com>
2019-04-02 21:13:05 +0800

    change bg-scale to bg-fill

--- scripts/mirror-display.sh
+++ scripts/mirror-display.sh
@@ -34,7 +34,7 @@ done
 
 compton --config $HOME/.config/compton/config.conf
 xrdb $HOME/.Xresources
-feh --bg-scale $HOME/Pictures/wallpapers/wallpaper.jpg
+feh --bg-fill $HOME/Pictures/wallpapers/wallpaper.jpg
 bash $HOME/.config/i3/polybar.sh &
 bash $HOME/.config/conky/reinitialize-conky.sh
 
