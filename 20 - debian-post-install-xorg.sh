dc5bf208841c4d9ef03e55670b0aa00d8ce0a376
Kevin Baisas <kevin.baisas@gmail.com>
2019-04-07 13:26:46 +0800

    debian: fix xinput is not included

--- 20 - debian-post-install-xorg.sh
+++ 20 - debian-post-install-xorg.sh
@@ -18,12 +18,13 @@ sudo apt install -y xfonts-utils
 # xbacklight - Adjust backlight brightness using RandR extension .
 sudo apt install -y xbacklight
 # xmodmap - Utility for modifying keymaps and pointer button mappings in X.
-# xinput  - Utility to configure and test X input devices, such as mouses,
-#           keyboards, and touchpads.
 # xrandr  - Used to set the size, orientation or reflection of the outputs for a screen.
 #           For multiple monitors, visit https://wiki.archlinux.org/index.php/Multihead
 # xrdb    - X server resource database utility.
 sudo apt install -y --no-install-recommends x11-xserver-utils
+# xinput  - Utility to configure and test X input devices, such as mouses,
+#           keyboards, and touchpads.
+sudo apt install -y --no-install-recommends xinput
 # xprop    - Property displayer for X.
 # xdpyinfo - Display information utility.
 sudo apt install -y --no-install-recommends x11-utils
