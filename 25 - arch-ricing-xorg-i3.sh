e6f5d81f5a9d1716c92a09dc2b01941b68aeea58
Kevin Baisas <kevin.baisas@gmail.com>
2019-04-10 03:25:19 +0800

    add dependencies

--- 25 - arch-ricing-xorg-i3.sh
+++ 25 - arch-ricing-xorg-i3.sh
@@ -495,7 +495,7 @@ Minimal installation done. Would you like to proceed [Yn]?   " yn
       yes | sudo pacman -S papirus-icon-theme
 
       # display
-      yes | sudo pacman -S feh arandr lxappearance-gtk3 xorg-xbacklight xorg-xrandr
+      yes | sudo pacman -S feh arandr lxappearance-gtk3 xorg-xbacklight xorg-xrandr xorg-xrdb xorg-xinput
 
       # package manager - arch
       # yes | yay -S pamac-tray-appindicator pamac-aur --noconfirm
