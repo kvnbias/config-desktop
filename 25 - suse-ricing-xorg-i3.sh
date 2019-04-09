4757385404427be5ff1491bb86769c270507507a
Kevin Baisas <kevin.baisas@gmail.com>
2019-04-09 20:05:46 +0800

    add trrs support

--- 25 - suse-ricing-xorg-i3.sh
+++ 25 - suse-ricing-xorg-i3.sh
@@ -308,39 +308,39 @@ Installation done...
 done
 
 ## TRRS
-#while true; do
-#  read -p "
-#Detect microphone plugged in a 4-pin 3.5mm (TRRS) jack [Yn]?   " yn
-#  case $yn in
-#    [Nn]* ) break;;
-#    * )
-#      while true; do
-#        echo "
-#Devices:
-#"
-#        aplay --list-devices
-#        read -p "
-#
-#
-#Check your HD Audio model here:
-#https://dri.freedesktop.org/docs/drm/sound/hd-audio/models.html or
-#http://git.alsa-project.org/?p=alsa-kernel.git;a=blob;f=Documentation/sound/alsa/HD-Audio-Models.txt;hb=HEAD
-#
-#Enter HD Audio Model (e.g. mbp101, macbook-pro, laptop-dmic etc) or [e]xit:   " hdam
-#        case $hdam in
-#          [Ee] ) break 2;;
-#          * )
-#            sudo touch /etc/modprobe.d/alsa-base.conf;
-#            echo "
-#options snd_hda_intel index=0
-#options snd_hda_intel model=$hdam" | sudo tee /etc/modprobe.d/alsa-base.conf
-#            break;;
-#        esac
-#      done;;
-#  esac
-#done
-#
-#
+while true; do
+  read -p "
+Detect microphone plugged in a 4-pin 3.5mm (TRRS) jack [Yn]?   " yn
+  case $yn in
+    [Nn]* ) break;;
+    * )
+      while true; do
+        echo "
+Devices:
+"
+        aplay --list-devices
+        read -p "
+
+
+Check your HD Audio model here:
+https://dri.freedesktop.org/docs/drm/sound/hd-audio/models.html or
+http://git.alsa-project.org/?p=alsa-kernel.git;a=blob;f=Documentation/sound/alsa/HD-Audio-Models.txt;hb=HEAD
+
+Enter HD Audio Model (e.g. mbp101, macbook-pro, laptop-dmic etc) or [e]xit:   " hdam
+        case $hdam in
+          [Ee] ) break 2;;
+          * )
+            sudo touch /etc/modprobe.d/alsa-base.conf;
+            echo "
+options snd_hda_intel index=0
+options snd_hda_intel model=$hdam" | sudo tee /etc/modprobe.d/alsa-base.conf
+            break;;
+        esac
+      done;;
+  esac
+done
+
+
 #if [ -d /etc/gdm ]; then
 #  # use lightdm instead
 #  sudo systemctl disable gdm
