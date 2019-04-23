04a31f6bed4229c2a026f15f7bd86d1081c67c7d
Kevin Baisas <kevin.baisas@gmail.com>
2019-04-23 18:28:34 +0800

    initial restructuring

--- setup-scripts/update-scripts.sh
+++ setup-scripts/update-scripts.sh
@@ -4,6 +4,7 @@ cp $(pwd)/scripts/volume-manager.sh                   $HOME/.config/audio/volume
 cp $(pwd)/scripts/brightness-manager.sh               $HOME/.config/display/brightness-manager.sh
 cp $(pwd)/scripts/lockscreen.sh                       $HOME/.config/display/lockscreen.sh
 cp $(pwd)/scripts/mirror-display.sh                   $HOME/.config/display/mirror-display.sh
+cp $(pwd)/scripts/screen-detector.sh                  $HOME/.config/display/screen-detector.sh
 cp $(pwd)/scripts/use-single-display.sh               $HOME/.config/display/use-single-display.sh
 cp $(pwd)/scripts/generate-conky-config.sh            $HOME/.config/conky/generate-conky-config.sh
 cp $(pwd)/scripts/generate-conky-helper.sh            $HOME/.config/conky/generate-conky-helper.sh
@@ -31,6 +32,7 @@ sudo chmod +x $HOME/.config/audio/volume-manager.sh
 sudo chmod +x $HOME/.config/display/brightness-manager.sh
 sudo chmod +x $HOME/.config/display/lockscreen.sh
 sudo chmod +x $HOME/.config/display/mirror-display.sh
+sudo chmod +x $HOME/.config/display/screen-detector.sh
 sudo chmod +x $HOME/.config/display/use-single-display.sh
 sudo chmod +x $HOME/.config/conky/generate-conky-config.sh
 sudo chmod +x $HOME/.config/conky/generate-conky-helper.sh
@@ -47,7 +49,7 @@ sudo chmod +x $HOME/.config/network/update-mirrors.sh
 sudo chmod +x $HOME/.config/touchpad/toggle-touchpad.sh
 sudo chmod +x $HOME/.config/polybar/popup-calendar.sh
 sudo chmod +x $HOME/.config/polybar/update-checker.sh
-sudo chmod +x $HOME/.config/themes/change-theme.sh
+sudo chmod +x $HOME/.config/themes/check-space.sh
 sudo chmod +x $HOME/.config/themes/change-theme.sh
 sudo chmod +x $HOME/.config/themes/update-polybar-network-interface.sh
 sudo chmod +x $HOME/.config/vifm/scripts/vifm-run.sh
