1fb22812c071041f2d39666b44a90f7796a94ebf
Kevin Baisas <kevin.baisas@gmail.com>
2019-04-25 21:45:11 +0800

    fix update checker: prevent package manager execute every polybar refresh

--- user-scripts/update-checker.sh
+++ user-scripts/update-checker.sh
@@ -1,52 +1,72 @@
+
 #!/bin/sh
 os=$(echo -n $(cat /etc/*-release 2> /dev/null | grep ^ID= | sed -e "s/ID=//"))
 updates=0
 
 if [ -f /usr/bin/pacman ]; then
-  if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
-    updates_arch=0
-  fi
+  manexec=$(ps aux | grep "checkupdates" 2> /dev/null | wc -l)
+  if [ "$manexec" -lt 2 ]; then
+    if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+      updates_arch=0
+    fi
 
-  if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
-    updates_aur=0
-  fi
+    if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
+      updates_aur=0
+    fi
 
-  updates=$(("$updates_arch" + "$updates_aur"))
+    updates=$(("$updates_arch" + "$updates_aur"))
+  fi
 
 elif [ -f /usr/bin/dnf ]; then
-  dnf=$(sudo dnf upgrade --refresh --assumeno 2> /dev/null)
+  manexec=$(ps aux | grep "dnf upgrade" 2> /dev/null | wc -l)
+  if [ "$manexec" -lt 2 ]; then
+    dnf=$(sudo dnf upgrade --refresh --assumeno 2> /dev/null)
 
-  upgrade=$(echo "$dnf" | grep '^Upgrade ' | awk '{ print $2 }')
-  install=$(echo "$dnf" | grep '^Install ' | awk '{ print $2 }')
+    upgrade=$(echo "$dnf" | grep '^Upgrade ' | awk '{ print $2 }')
+    install=$(echo "$dnf" | grep '^Install ' | awk '{ print $2 }')
 
-  updates=$(( upgrade + install ))
+    updates=$(( upgrade + install ))
+  fi
 
 elif [ -f /usr/bin/zypper ]; then
-  refresh=$(sudo zypper refresh > /dev/null)
-  updates=$(sudo zypper list-updates | grep -v "+-" | grep -v "Available Version" | grep -v "\.\.\." | grep -v "No updates found." | wc -l 2> /dev/null);
+  manexec=$(ps aux | grep "zypper refresh" 2> /dev/null | wc -l)
+  if [ "$manexec" -lt 2 ]; then
+    refresh=$(sudo zypper refresh > /dev/null)
+    updates=$(sudo zypper list-updates | grep -v "+-" | grep -v "Available Version" | grep -v "\.\.\." | grep -v "No updates found." | wc -l 2> /dev/null);
 
-  if echo "$updates" | grep "No updates found."; then
-    updates=0
+    if echo "$updates" | grep "No updates found."; then
+      updates=0
+    fi
   fi
 
 elif [ -f /usr/bin/emerge ]; then
-  lastdate=$(date -f  /usr/portage/metadata/timestamp.chk +%j)
-  currdate=$(date +%j)
-  diffdate=$(( currdate - lastdate ))
+  manexec=$(ps aux | grep "emerge --pretend" 2> /dev/null | wc -l)
+  echo "here"
+  echo "$manexec"
+  if [ "$manexec" -lt 2 ]; then
+    echo "$maxexec"
+    lastdate=$(date -f  /usr/portage/metadata/timestamp.chk +%j)
+    currdate=$(date +%j)
+    diffdate=$(( currdate - lastdate ))
 
-  if [ "$diffdate" -gt 2 ]; then
-    $(sudo emerge --sync > /dev/null)
+    if [ "$diffdate" -gt 2 ]; then
+      $(sudo emerge --sync > /dev/null)
+    fi
+
+    updates=$(sudo emerge --pretend --verbose --update --deep --newuse @world | grep "\[ebuild.*\]" 2> /dev/null | wc -l)
   fi
-
-  updates=$(sudo emerge --pretend --verbose --update --deep --newuse @world | grep "\[ebuild.*\]" 2> /dev/null | wc -l)
 
 elif [ -f /usr/bin/apt ]; then
-  apt=$(sudo apt update 2> /dev/null)
+  manexec=$(ps aux | grep "apt update" 2> /dev/null | wc -l)
+  if [ "$manexec" -lt 2 ]; then
+    apt=$(sudo apt update 2> /dev/null)
 
-  updates=0
-  if echo "$apt" | grep -q "upgraded"; then
-    updates=$(echo "$apt" | grep 'upgraded' | awk '{ print $1 }')
+    updates=0
+    if echo "$apt" | grep -q "upgraded"; then
+      updates=$(echo "$apt" | grep 'upgraded' | awk '{ print $1 }')
+    fi
   fi
+
 fi
 
 if [ "$updates" -gt 0 ]; then
