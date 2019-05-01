f8bf3d03838e9d12550b0807f77f01fef9197a97
Kevin Baisas <kevin.baisas@gmail.com>
2019-05-02 04:59:57 +0800

    rhel: add startup.nsh script creation

--- install-scripts/fedora/10-rhel-post-install.sh
+++ install-scripts/fedora/10-rhel-post-install.sh
@@ -85,6 +85,22 @@ while true; do
   esac
 done
 
+if [ -d /sys/firmware/efi/efivars ] && sudo test ! -f "/boot/efi/startup.nsh"; then
+  sudo mkdir -p /boot/efi/EFI/boot
+  if [ -d "/boot/efi/EFI/refind" ]; then
+    sudo cp -a /boot/efi/EFI/refind/refind_x64.efi /boot/efi/EFI/boot/bootx64.efi
+  elif [ -d "/boot/efi/EFI/grub" ]; then
+    sudo cp -a /boot/efi/EFI/grub/grubx64.efi /boot/efi/EFI/boot/bootx64.efi
+  elif [ -d "/boot/efi/EFI/GRUB" ]; then
+    sudo cp -a /boot/efi/EFI/GRUB/grubx64.efi /boot/efi/EFI/boot/bootx64.efi
+  else
+    sudo cp -a /boot/efi/EFI/$os/grubx64.efi /boot/efi/EFI/boot/bootx64.efi
+  fi
+
+  echo "bcf boot add 1 fs0:\\EFI\\boot\\bootx64.efi \"Fallback Bootloader\"
+exit" | sudo tee /boot/efi/startup.nsh
+fi
+
 if [ "$os" = "fedora" ]; then
   sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$fedver.noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$fedver.noarch.rpm
 else
--- install-scripts/fedora/20-rhel-post-install-xorg.sh
+++ install-scripts/fedora/20-rhel-post-install-xorg.sh
@@ -504,6 +504,22 @@ Inherits=Breeze
 }
 
 #### PREP DESKTOP ENV
+if [ -d /sys/firmware/efi/efivars ] && sudo test ! -f "/boot/efi/startup.nsh"; then
+  sudo mkdir -p /boot/efi/EFI/boot
+  if [ -d "/boot/efi/EFI/refind" ]; then
+    sudo cp -a /boot/efi/EFI/refind/refind_x64.efi /boot/efi/EFI/boot/bootx64.efi
+  elif [ -d "/boot/efi/EFI/grub" ]; then
+    sudo cp -a /boot/efi/EFI/grub/grubx64.efi /boot/efi/EFI/boot/bootx64.efi
+  elif [ -d "/boot/efi/EFI/GRUB" ]; then
+    sudo cp -a /boot/efi/EFI/GRUB/grubx64.efi /boot/efi/EFI/boot/bootx64.efi
+  else
+    sudo cp -a /boot/efi/EFI/$os/grubx64.efi /boot/efi/EFI/boot/bootx64.efi
+  fi
+
+  echo "bcf boot add 1 fs0:\\EFI\\boot\\bootx64.efi \"Fallback Bootloader\"
+exit" | sudo tee /boot/efi/startup.nsh
+fi
+
 # fonts
 if [ ! -f /etc/X11/xorg.conf ];then
   sudo touch /etc/X11/xorg.conf;
