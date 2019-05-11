84c4ec643b1ebba08e56280e33c224b5a0f898c1
Kevin Baisas <kevin.baisas@gmail.com>
2019-05-12 00:12:15 +0800

    udpate submodules-fetch script

--- submodules-fetch.sh
+++ submodules-fetch.sh
@@ -2,10 +2,24 @@
 #!/bin/bash
 DIR="$(cd "$( dirname "$0" )" && pwd)"
 
+comb=false
+while true; do
+  read -p "Checkout to master branch on all submodules [yN]?   " combs
+  case $combs in
+    [Yy]* ) comb=true; break;;
+    * ) break;;
+  esac
+done
+
 while true; do
   read -p "Fetch 'theme' submodule in this project [yN]?   " ftsp
   case $ftsp in
-    [Yy]* ) git submodule update --init themes; break;;
+    [Yy]* )
+      git submodule update --init themes;
+      if [ "$comb" = true ]; then
+        cd "$DIR/themes" && git checkout master
+      fi
+      break;;
     * ) break;;
   esac
 done
@@ -18,8 +32,18 @@ while true; do
         read -p "Fetch 'private wallpapers' submodule in this project (Needs access on the repo) [yN]?   " ftsp
         case $ftsp in
           [Yy]* )
-            git submodule update --init rice/images; cd "$DIR/rice/images";
-            git submodule update --init wallpapers/private; cd "$DIR";
+            git submodule update --init rice/images;
+            if [ "$comb" = true ]; then
+              cd "$DIR/rice/images" && git checkout master
+            fi
+
+            git submodule update --init wallpapers/private;
+
+            if [ "$comb" = true ]; then
+              cd "$DIR/rice/images/wallpapers/private" && git checkout master;
+            fi
+
+            cd "$DIR"
             break 2;;
           * )
             git submodule update --init rice/images;
@@ -34,7 +58,12 @@ done
 while true; do
   read -p "Fetch 'icon-themes' submodule in this project (Takes a lot of space & will slow down editors) [yN]?   " ftsp
   case $ftsp in
-    [Yy]* ) git submodule update --init icon-themes; break;;
+    [Yy]* )
+      git submodule update --init icon-themes;
+      if [ "$comb" = true ]; then
+        cd "$DIR/icon-themes" && git checkout master
+      fi
+      break;;
     * ) break;;
   esac
 done
@@ -42,7 +71,12 @@ done
 while true; do
   read -p "Fetch 'dev' submodule in this project [yN]?   " ftsp
   case $ftsp in
-    [Yy]* ) git submodule update --init dev; break;;
+    [Yy]* )
+      git submodule update --init dev;
+      if [ "$comb" = true ]; then
+        cd "$DIR/dev" && git checkout master
+      fi
+      break;;
     * ) break;;
   esac
 done
